95001f2c9abc7dfba7cafaba5f9043ba93b451f8
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-05-21 14:35:52 -0700

    final commit

--- leap-test.py
+++ leap-test.py
@@ -2,6 +2,8 @@
 
 # Tests for leap.py.
 
+import pytest
+
 from leap import is_leap_year
 
 
@@ -9,3 +11,11 @@ def test_is_leap1():
     assert is_leap_year("2020") == True
 
 
+def test_is_leap2():
+    assert is_leap_year("2021") == False
+
+def test_is_leap3():
+    with pytest.raises(ValueError):
+        is_leap_year("fasdkjgfas")
+        
+    
